dcbbd54b79d4b9d41ac8d8076feea56c76a2d368
S.G <samat.gaynullin@yandex.ru>
2026-03-05 15:08:47 +0300

    hpp updated

--- SRC/console_ui.hpp
+++ SRC/console_ui.hpp
@@ -3,13 +3,6 @@
 #include <iostream>
 
 class console_UI {
-private:
-    struct return_data {
-        std::string title_split_line = "";
-        int center_textsplitline_index = 0;
-
-        std::string text_split_line = "";
-    };
 public:
     struct window_settings {
         window_settings(std::string title = "", std::string text = "") {
@@ -26,8 +19,15 @@ public:
 
     
     static std::string window_get(const window_settings &window, bool user_feedback = false);
-    static return_data calculate_vars(const console_UI::window_settings &window);
-
     static std::string input(unsigned int maxAttempts = 10);
 
+private:
+    struct return_data {
+        std::string title_split_line = "";
+        int center_textsplitline_index = 0;
+
+        std::string text_split_line = "";
+    };
+
+    static return_data calculate_vars(const console_UI::window_settings &window);
 };
